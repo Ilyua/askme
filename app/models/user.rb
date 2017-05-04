e36bffb0116a4168dcc3d0466a458f429bcd7fcd
agrestid <old.flanigan@gmail.com>
2017-05-04 19:46:03 +0300

    Add some validators for user and question

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,46 @@
+require 'openssl'
+
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ApplicationRecord
+  ITERATIONS = 20000
+  DIGEST = OpenSSL::Digest::SHA256.new
+  has_many :questions
+  validates :username ,length: {maximum: 40},format:{with: /[a-zA-z0-9_]/, message:"not allows several symbols"}
+  validates :email, :username, presence: true,email:true
+  validates :email, :username, uniqueness: true
+  attr_accessor :password
+
+  validates_presence_of :password, on: :create
+  validates_confirmation_of :password
+  before_save :encrypt_password
+
+  def encrypt_password
+    if self.password.present?
+      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+      self.password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
+      )
+    end
+  end
+
+  def self.hash_to_string(password_hash)
+    password_hash.unpack('H*')[0]
+  end
+
+  def self.authenticate(email, password)
+    user = find_by(email: email)
+    if user.present? && user.password_hash == User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST))
+      user
+    else
+      nil
+    end
+  end
+
 end
