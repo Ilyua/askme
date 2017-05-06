136e5142985d234fb1a6f5d2685e12f1481af787
drapes <tihon1997@gmail.com>
2017-05-06 20:00:57 +0300

    styles, numebr of questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,33 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+        User.new(
+            id: 1,
+            name: 'One',
+            username: 'One',
+            avatar_url: 'photo'
+        )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
 
   def show
-    @hello = "Hello, world!"
+    @user = User.new(
+        name: 'Ilya',
+        username: 'Finnegan',
+        avatar_url: ''
+    )
+
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+        Question.new(text: 'Что делаешь?', created_at: Date.parse('29.03.2017'))
+    ]
+
+    @new_question = Question.new
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar_url.present?
+      user.avatar_url
+    else
+      asset_path 'anon.png'
+    end
+  end
+
+  def sklonenie(number, krokodil, krokodila, krokodilov)
+    if number == nil || !number.is_a?(Numeric)
+      return nil
+    end
+
+    ostatok_first = number % 100
+
+    return krokodilov if (11..14).include?(ostatok_first)
+
+    ostatok_second = number % 10
+
+    return krokodil if ostatok_second == 1
+
+    return krokodila if (2..4).include?(ostatok_second)
+
+    return krokodilov if ostatok_second > 4 || ostatok_second == 0
+  end
 end
